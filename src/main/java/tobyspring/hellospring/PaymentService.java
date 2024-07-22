2e3b6659bd6256598a3d57d17a5915ac3bc1784c
Yongbb_13 <hvyongkim2@naver.com>
2024-07-22 16:19:04 +0900

    인프런 토비스프링 원칙과 패턴

---
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -0,0 +1,38 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+public class PaymentService {
+
+    private final ExRateProvider exRateProvider;
+
+    public PaymentService(ExRateProvider exRateProvider){
+        this.exRateProvider = exRateProvider;
+    }
+
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+
+        BigDecimal exRate = exRateProvider.getExRate(currency);
+        // 금액 계산
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        // 유효 시간 계산
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount
+                , exRate
+                , convertedAmount
+                , validUntil);
+    }
+
+}
